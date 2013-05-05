e4786818f68bc47c9f9b5efcd505374ec1cdca9c
mikeholbrook <holbrookexteriors@gmail.com>
2013-05-05 20:14:50 -0300

    Update 00_nearest_larger.rb

--- coding-test-2/practice-problems/lib/00_nearest_larger.rb
+++ coding-test-2/practice-problems/lib/00_nearest_larger.rb
@@ -1,2 +1,21 @@
-def nearest_larger(arr, idx)
+def nearest_larger(arr,i)
+# i=8
+#arr=[2,4,4,3,4,4,8,4,8]
+dist=0
+while dist<(arr.count)
+      if (i-dist >= 0) && (arr[i-dist] > arr[i])
+    return(i-dist)
+    break
+  end
+  if (i+dist <= arr.count-1) && (arr[i+dist] > arr[i])
+    return(i+dist)
+    break
+  end
+  dist=dist+1
 end
+puts nil
+
+end
+
+
+
